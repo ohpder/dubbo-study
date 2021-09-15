a95d405a2e7ddba141ce664195559a9641aad073
nlsq <1402023451@qq.com>
2021-09-15 10:25:05 +0800

    initial commit

---
+++ dubbo-producter/src/main/java/com/example/dubboproducter/DubboProducterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubboproducter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProducterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProducterApplication.class, args);
+    }
+
+}
---
+++ dubbo-producter/src/test/java/com/example/dubboproducter/DubboProducterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dubboproducter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboProducterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
